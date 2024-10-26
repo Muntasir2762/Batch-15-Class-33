f67f5bb30fcf3fe49b2127d70edb546c95755e1e
Munotasir2762 <muntasirpranto2762@gmail.com>
2024-10-26 23:46:12 +0600

    Js function

---
+++ script.js
@@ -0,0 +1,39 @@
+
+function sum (number1, number2)
+{
+    let x = number1; //78
+    let y = number2; //80
+
+    let result = x+y;
+
+    // console.log(result); // 144
+    return result;
+}
+
+function sub (number1, number2)
+{
+    let x = number1;
+    let y = number2;
+
+    let result = number1 - number2;
+
+    return result;
+}
+
+//console.log(sum (54, 90)); //144
+
+let summation = sum (100, 300); //400
+
+console.log(summation);
+
+let secondSum = summation + 120
+console.log(secondSum);
+
+
+let thirdSum = sum (20, 56); //
+console.log (thirdSum);
+
+
+let subtraction = sub (100, 38);
+
+console.log (subtraction);
